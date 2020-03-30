c06f35ec501b3c56f145418db0f5129e848c0395
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-30 15:46:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
-class CostumeStores < ActiveRecord
+class CostumeStore < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |cs|
+      cs.string :name 
+      cs.string :location
+      cs.integer :costume_inventory
+      cs.integer :employees_count
+      cs.boolean :in_bussiness?
+      cs.timestamps
+    end
+  end
+end
