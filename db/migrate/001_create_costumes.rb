cda6505ec247785c0908225e8f27dec95d1ebd93
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-30 15:35:58 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |c|
     c.string :name
-    c.
+    c.integer :price
+    c.integer :size 
+    c.string :image_url
   end
 end
 
